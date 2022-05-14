29c980310adc9988ad07e6fc94743d7ac88cf471
Indra W <indra.roadota@gmail.com>
2022-05-14 21:05:29 +0700

    style

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 const mix = require('laravel-mix');
 
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -15,3 +16,4 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
+mix.sass('resources/sass/app.scss', 'public/css')

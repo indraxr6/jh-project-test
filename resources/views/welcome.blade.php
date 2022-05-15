b7f03739d0adabba7e18e679e0f248748a6e6557
Indra W <indra.roadota@gmail.com>
2022-05-15 20:59:43 +0700

    ch3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,31 +1,33 @@
-
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Website Gratis, 1 Hari Jadi - Sitemaya</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    
-    <style>
-      
-      /* .navbar-nav > li{
-  margin-left:30px;
-  margin-right:30px;
-} */
-    </style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Website Gratis, 1 Hari Jadi - Sitemaya</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/8a8bc43b9c.js" crossorigin="anonymous"></script>
+  <style>
+    /* .navbar-nav > li{
+    margin-left:30px;
+    margin-right:30px;
+  } */
+  </style>
 </head>
+
 <body>
   <header>
     <!--- Navbar --->
     <nav class="navbar navbar-expand-lg navbar-light bg-white my-2 my-lg-2">
       <div class="container">
-      <img src="https://sitemaya.com/wp-content/uploads/2021/10/Sitemaya-Export-01-204x49.png" width=204 alt="">
-      <a class="navbar-brand" ></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+        <img src="https://sitemaya.com/wp-content/uploads/2021/10/Sitemaya-Export-01-204x49.png" width=204 alt="">
+        <a class="navbar-brand"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
         <div class="collapse navbar-collapse" id="nvbCollapse">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
@@ -35,7 +37,8 @@
               <a class="nav-link" href="#">Pricing</a>
             </li>
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
                 Features
               </a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -50,30 +53,214 @@
                 <a class="dropdown-item" href="#">Animasi</a>
               </div>
             </li>
-           <li class="nav-item">
+            <li class="nav-item">
               <a class="nav-link" href="#">Pilihan Template</a>
             </li>
-            <button style="background-color: white; padding-bottom: 30px" type="button" class="btn btn-outline-dark rounded 0 login">Login</button>
-            <button style="padding-bottom: 30px" type="button" class="btn btn-outline-dark rounded 0 ">Daftar Gratis</button>
+            <button style="background-color: white; padding-bottom: 30px" type="button"
+              class="btn btn-outline-dark rounded 0 login">Login</button>
+            <button style="padding-bottom: 30px" type="button" class="btn btn-outline-dark rounded 0 ">Daftar
+              Gratis</button>
           </ul>
         </div>
       </div>
     </nav>
     <!--# Navbar #-->
-    </header>
+  </header>
 
-   <div>
-    <img src="https://cdn.sitemaya.com/wp-content/uploads/2021/10/21144940/imagination-creativity-illustrator-5153623.jpg" class="rounded float-left" width="37%" alt="...">
+  <div>
+    <img
+      src="https://cdn.sitemaya.com/wp-content/uploads/2021/10/21144940/imagination-creativity-illustrator-5153623.jpg"
+      class="rounded float-left" width="37%" alt="...">
 
-    <h1>h1. Bootstrap heading</h1>
-   </div>
-  
-  
-  
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <br><br><br>
+
+    <div>
+      <div class="uptext">
+        <h1>BEBAS BERKREASI</h1>
+        <span>DENGAN WEBSITE GRATIS</span>
+
+        <div>
+          <div>
+            <p>
+              Beneran! Tersedia website gratis dengan builder dan <strong>template premium</strong> yang tinggal kamu
+              pakai sesukamu. Dan yang pasti, cepat diakses dimanapun, dari koneksi apapun.
+            </p>
+          </div>
+          <br>
+          <div style="list-style: none; font-family: 'Urbanist', sans-serif">
+            <div class="tick-container">
+              <li class="">
+                <i class="fa-solid fa-check-double"></i>Drag & Drop
+              </li>
+            </div>
+            <div class="tick-container">
+              <li class="">
+                <i class="fa-solid fa-check-double"></i>Berbasis WordPress
+              </li>
+            </div>
+            <div class="tick-container">
+              <li class="">
+                <i class="fa-solid fa-check-double"></i>Ratusan Template Premium
+              </li>
+            </div>
+            <div class="tick-container">
+              <li class="">
+                <i class="fa-solid fa-check-double"></i>Gratis SSL
+              </li>
+            </div>
+          </div>
+          <br>
+          <button style="padding: 10px" type="button" class="regbutton ">BUAT WEBSITE GRATIS SEKARANG <i
+              class="fa-solid fa-angle-right"></i></button>
+
+        </div>
+
+      </div>
+    </div>
+
+    <div>
+
+      <img
+        src="https://cdn.sitemaya.com/wp-content/uploads/2021/10/21163908/imagination-fairy-tale-creativity-5153649.jpg"
+        class="rounded float-right" width="27%" alt="...">
+      <div class="text-left ml-5">
+        <h1>BEBAS BERKREASI</h1>
+        <p style="font-size: 21px">
+          Website ini juga dibuat dengan website builder gratis kami. Wujudkan website impianmu disini, mulai dengan
+          gratis.
+        </p>
+      </div>
+      <br>
+
+      <div class="grid2">
+        <div style="text-shadow: 5px 5px 0px #00ffae; font-size: 28px; list-style: none" class="container ml-5">
+          <div class="row">
+            <div class="col">
+              <h2><i class="fa-solid fa-rocket"></i> Lebih Cepat Pakai CDN</h2>
+              <p style="font-size: 16px; text-shadow: 0px 0px 0px #00ffae">
+                Dengan server CDN kami yang berada di berbagai penjuru dunia, kami bisa pastikan website gratis mu lebih
+                cepat hingga 200%. Ditambah, website gratis mu juga akan punya uptime hingga 99,9%.
+              </p>
+            </div>
+            <div class="col">
+
+              <h2><i class="fa-solid fa-arrow-pointer"></i> Drag & Drop Builder</h2>
+              <p style="font-size: 16px; text-shadow: 0px 0px 0px #00ffae">
+                Bikin website semudah menata interior. Tanpa coding, tinggal klik sana-sini, kamu udah bisa bikin
+                website keren macam website ini. Iya, website ini dibuat oleh orang awam tanpa background programming.
+              </p>
+
+            </div>
+
+          </div>
+          <div class="row">
+            <div class="col">
+              <h2><i class="fa-brands fa-wordpress-simple"></i> Berbasis WordPress</h2>
+              <p style="font-size: 16px; text-shadow: 0px 0px 0px #00ffae">
+                Web builder kami berbasis wordpress yang telah digunakan jutaan orang di dunia. Sudah terbukti, apalagi
+                kami menyediakan template & plugin premium secara GRATIS.
+              </p>
+
+            </div>
+            <div class="col">
+              <h2><i class="fa-solid fa-paint-roller"></i> Template Premium</h2>
+              <p style="font-size: 16px; text-shadow: 0px 0px 0px #00ffae">
+                Biasanya, kamu perlu membayar hingga ratusan ribu untuk pakai template premium. Nah, saat kamu buat
+                website disini, kamu bisa menggunakan RATUSAN TEMPLATE PREMIUM secara GRATIS.
+              </p>
+
+            </div>
+
+          </div>
+          <div class="row">
+            <div class="col">
+              <h2><i class="fa-solid fa-images"></i> Stok Foto Premium</h2>
+              <p style="font-size: 16px; text-shadow: 0px 0px 0px #00ffae">
+                Ribuan gambar & foto premium yang kami berikan secara GRATIS dapat kamu gunakan langsung di dalam
+                website builder. Ga perlu repot download-upload di komputermu.
+              </p>
+
+            </div>
+            <div class="col">
+              <h2><i class="fa-solid fa-plug"></i> Plugin Premium</h2>
+              <p style="font-size: 16px; text-shadow: 0px 0px 0px #00ffae">
+                Kamu pengguna wordpress? Jika iya, maka kini kamu bisa mendapatkan plugin-plugin premium kesayanganmu
+                GRATIS sesuai dengan paket yang kamu pilih saat mendaftar. Tinggi pilih, lalu klik install.
+              </p>
+
+            </div>
+          </div>
+          <br>
+          <button style="padding: 10px; font-size : 13px;" type="button"
+            class="regbutton btn justify-content-center ">BUAT WEBSITE SEKARANG <i
+              class="fa-solid fa-angle-right"></i></button>
+          <p style="font-size: 12px; text-shadow: 0px 0px 0px #00ffae; margin-left: 50px">
+            <a style="text-decoration: none" href="#">Lihat Fitur Lengkap </a><i class="fa-solid fa-angle-right"></i>
+          </p>
+        </div>
+
+         <div style="margin-top: 5%" class="text-left ml-5 pt-5">
+        <h1 style="text-shadow: 0px 0px 0px #00ffae; color: white; font-size: 48px;"><mark style="background: black; color: white;">BUAT WEBSITE CANGGIH?</mark></h1>
+        
+        <h1 style="text-shadow: 2px 5px 0px #ffffff;; color: black; font-size: 56px;"><mark style="background: #00ffae"> BISA BANGET</mark></h1>
+
+        <p style="margin-left: 50%; display: inline-block; font-size: 16px" class="text-left mr-5 pl-5 ">Kami menyediakan banyak integrasi dengan plugin-plugin premium wordpress yang sudah terbukti digunakan banyak orang. Plugin-plugin ini akan membantu kamu untuk membuat website sistem yang canggih, sesuai dengan kebutuhan bisnismu..</p>
+        
+      </div>
+      </div>
       
+      <div class="container mt-5">
+        <div class="row mx-auto">
+          <div class="col-sm ">
+            <h2 style="text-shadow: 0px 0px 0px #00ffae">SEO Management</h2>
+              <p style="font-size: 16px; text-shadow: 0px 0px 0px #00ffae">
+                Manfaatkan plugin SEO premium untuk membangun SEO on page websitemu dengan mudah, dilengkapi dengan fitur analisa dan auto-suggestion.
+              </p>
+        </div>
+        <div class="col-sm ">
+          <h2 style="text-shadow: 0px 0px 0px #00ffae">SEO Management</h2>
+            <p style="font-size: 16px; text-shadow: 0px 0px 0px #00ffae">
+              Manfaatkan plugin SEO premium untuk membangun SEO on page websitemu dengan mudah, dilengkapi dengan fitur analisa dan auto-suggestion.
+            </p>
+      </div>
+      <div class="col-sm ">
+        <h2 style="text-shadow: 0px 0px 0px #00ffae">SEO Management</h2>
+          <p style="font-size: 16px; text-shadow: 0px 0px 0px #00ffae">
+            Manfaatkan plugin SEO premium untuk membangun SEO on page websitemu dengan mudah, dilengkapi dengan fitur analisa dan auto-suggestion.
+          </p>
+    </div>
+    <div class="col-sm ">
+      <h2 style="text-shadow: 0px 0px 0px #00ffae">SEO Management</h2>
+        <p style="font-size: 16px; text-shadow: 0px 0px 0px #00ffae">
+          Manfaatkan plugin SEO premium untuk membangun SEO on page websitemu dengan mudah, dilengkapi dengan fitur analisa dan auto-suggestion.
+        </p>
+  </div>
+  
+        
+      </div>
+
+
+
+
+
+    </div>
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+      integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+      crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+      integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+      crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+      integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+      crossorigin="anonymous"></script>
+
 </body>
 
 </html>
